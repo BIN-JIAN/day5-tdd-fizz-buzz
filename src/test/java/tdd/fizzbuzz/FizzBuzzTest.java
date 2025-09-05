5539f198a16a41567f416120afa3188120550ec8
BIN <BIN.JIAN@OOCL.COM>
2025-09-05 11:35:49 +0800

    test

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,20 @@
 package tdd.fizzbuzz;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class FizzBuzzTest {
+
+  @Test
+  void Should_return_normal_number_when_countOff_then_normal_number(){
+    //Given
+    int number = 1;
+    FizzBuzz fizzBuzz = new FizzBuzz();
+    //when
+    String result = fizzBuzz.create(number);
+    //Then
+    assertEquals("1",result);
+  }
+
 }
