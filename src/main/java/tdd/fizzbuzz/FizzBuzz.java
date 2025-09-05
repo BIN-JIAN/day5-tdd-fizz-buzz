c7969ee92a2d7a05ae68018ce531cab048ab795b
BIN <BIN.JIAN@OOCL.COM>
2025-09-05 11:41:28 +0800

    feat  multiple_3

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,8 +1,11 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
-
+ public final String FIZZ="Fizz";
   public String create(int number) {
+    if(number%3 == 0){
+      return FIZZ;
+    }
     return String.valueOf(number);
   }
 }
